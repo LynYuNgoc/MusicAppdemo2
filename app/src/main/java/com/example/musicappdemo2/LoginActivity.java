f6b2605555d9c4eac38aae98a45459324b6901f0
LynYuNgoc <ngocnn7749@gmail.com>
2024-02-29 02:10:13 +0700

    giao dien login

--- app/src/main/java/com/example/musicappdemo2/LoginActivity.java
+++ app/src/main/java/com/example/musicappdemo2/LoginActivity.java
@@ -2,14 +2,29 @@ package com.example.musicappdemo2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btLogin = findViewById(R.id.buttonLogin);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                i.putExtra("","");
+                startActivity(i);
+            }
+        });
 
 
     }
