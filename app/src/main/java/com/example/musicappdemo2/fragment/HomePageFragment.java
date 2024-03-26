184e661ba544294ad2f7b062bb15d9bb37f282a8
mlbxiu <ngochuong224466@gmail.com>
2024-03-26 15:18:38 +0700

    banner

--- app/src/main/java/com/example/musicappdemo2/fragment/HomePageFragment.java
+++ app/src/main/java/com/example/musicappdemo2/fragment/HomePageFragment.java
@@ -6,12 +6,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.musicappdemo2.R;
+import com.example.musicappdemo2.classes.ImageBanner;
+import com.example.musicappdemo2.classes.ImageBannerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.relex.circleindicator.CircleIndicator;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +28,10 @@ import com.example.musicappdemo2.R;
  * create an instance of this fragment.
  */
 public class HomePageFragment extends Fragment {
+    private ViewPager viewPager;
+    private CircleIndicator circleIndicator;
+    private ImageBannerAdapter imageBannerAdapter;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -64,8 +77,74 @@ public class HomePageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+
+        viewPager = view.findViewById(R.id.viewPager);
+        circleIndicator = view.findViewById(R.id.circle_indicator);
+
+        imageBannerAdapter = new ImageBannerAdapter(getContext(), getListImgBanner());
+        viewPager.setAdapter(imageBannerAdapter);
+
+        circleIndicator.setViewPager(viewPager);
+        imageBannerAdapter.registerDataSetObserver(circleIndicator.getDataSetObserver());
+
+
+        // Tạo một Handler mới
+        final Handler handler = new Handler();
+        // Tạo một Runnable để thực hiện việc chuyển đổi ảnh trong ViewPager
+        final Runnable update = new Runnable() {
+            public void run() {
+                // Lấy vị trí của trang hiện tại
+                int currentPage = viewPager.getCurrentItem();
+                // Kiểm tra xem nếu trang hiện tại không phải là trang cuối cùng, thì chuyển đến trang kế tiếp
+                if (currentPage == imageBannerAdapter.getCount() - 1) {
+                    currentPage = 0;
+                } else {
+                    currentPage++;
+                }
+                // Chuyển đổi đến trang tiếp theo
+                viewPager.setCurrentItem(currentPage, true);
+            }
+        };
+
+        // Thiết lập lịch trình cho việc chuyển đổi ảnh sau mỗi 3 giây
+        handler.postDelayed(update, 3000);
+
+        // Sử dụng addOnPageChangeListener để xác định khi người dùng thực hiện thao tác trượt trang
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // Nếu trạng thái là SCROLL_STATE_IDLE (không cuộn), thì chờ 3 giây rồi chuyển đến trang kế tiếp
+                if (state == ViewPager.SCROLL_STATE_IDLE) {
+                    handler.postDelayed(update, 3000);
+                }
+            }
+        });
+
+        return view;
     }
+
+    private List<ImageBanner> getListImgBanner() {
+
+        List<ImageBanner> list = new ArrayList<>();
+        list.add(new ImageBanner(R.drawable.imgbanner_0));
+        list.add(new ImageBanner(R.drawable.imgbanner_1));
+        list.add(new ImageBanner(R.drawable.imgbanner_2));
+
+        return list;
+
+
+    }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -82,16 +161,19 @@ public class HomePageFragment extends Fragment {
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
         transaction.replace(R.id.frameLayoutHorizontalCategory, childFragment).commit();
     }
+
     void addArtist() {
         Fragment childFragment = new ListHomePageArtistFragment();
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
         transaction.replace(R.id.frameLayoutHorizontalArtist, childFragment).commit();
     }
+
     void addAlbumHot() {
         Fragment childFragment = new ListHomePageAlbumHotFragment();
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
         transaction.replace(R.id.frameLayoutHorizontalAlbumHot, childFragment).commit();
     }
+
     void addChill() {
         Fragment childFragment = new ListHomePageChillFragment();
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
